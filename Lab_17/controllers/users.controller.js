0de0629f0f390b28e66cd858e46e2fbf8e103eb2
Tony <111481792+Bowist27@users.noreply.github.com>
2024-03-18 23:30:24 -0600

    LAB17-Final

--- Lab_17/controllers/users.controller.js
+++ Lab_17/controllers/users.controller.js
@@ -1,3 +1,8 @@
 exports.get_login = (request, response, next) => {
     response.render('login');
 };
+exports.get_logout = (request, response, next) => {
+    request.session.destroy(() => {
+        response.redirect('/users/login'); //Este código se ejecuta cuando la sesión se elimina.
+    });
+};
