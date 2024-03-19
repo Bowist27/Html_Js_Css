b8aea484df100937114e9cf11601521cd9da775d
Tony <111481792+Bowist27@users.noreply.github.com>
2024-03-18 22:17:08 -0600

    LAB12-Carpeta-estatica

--- Lab_12/app.js
+++ Lab_12/app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,7 +18,6 @@ app.use((request, response, next) => {
 const rutasClases = require('./routes/clases.routes');
 app.use('/', rutasClases);
 
-const path = require('path');
 app.use((request, response, next) => {
   response.status(404);
   response.sendFile(path.join(__dirname, 'views', '404.html')); //Manda la respuesta
