b770f4e9609f4d4f890e445ea51514c89b86cb70
Tony <111481792+Bowist27@users.noreply.github.com>
2024-03-18 22:45:41 -0600

    LAB13-rutas

--- Lab_13/controllers/tropas.controller.js
+++ Lab_13/controllers/tropas.controller.js
@@ -1,3 +1,35 @@
+const tropas = [
+    {
+      clase: "Jett", 
+      nivel: 1, 
+      vida: 100, 
+      ataque: 30, 
+      imagen: "https://th.bing.com/th/id/OIP.UDgKrcYTQNBsOuSPed43_AHaFj?rs=1&pid=ImgDetMain",
+    },
+    {
+      clase: "Killjoy", 
+      nivel: 1, 
+      vida: 100, 
+      ataque: 30, 
+      imagen: "https://i.pinimg.com/originals/3b/5c/30/3b5c307c5f3b9bf12f89bf9b29f7eff0.jpg",
+    }
+];
 exports.get_crear = (request, response, next) => {
     response.render('crear');
+};
+exports.post_crear = (request, response, next) => {
+    console.log(request.body);
+    tropas.push({
+        clase: request.body.clase, 
+        nivel: 1, 
+        vida: request.body.vida, 
+        ataque: request.body.ataque, 
+        imagen: request.body.imagen,
+    });
+    response.redirect('/');
+};
+exports.get_root = (request, response, next) => {
+    response.render('clases', {
+        tropas: tropas,
+    });
 };
--- Lab_13/routes/clases.routes.js
+++ Lab_13/routes/clases.routes.js
@@ -2,43 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const tropasController = require('../controllers/tropas.controller');
-
-
-const tropas = [
-  {
-    clase: "Jett", 
-    nivel: 1, 
-    vida: 100, 
-    ataque: 30, 
-    imagen: "https://th.bing.com/th/id/OIP.UDgKrcYTQNBsOuSPed43_AHaFj?rs=1&pid=ImgDetMain",
-  },
-  {
-    clase: "Killjoy", 
-    nivel: 1, 
-    vida: 100, 
-    ataque: 30, 
-    imagen: "https://i.pinimg.com/originals/3b/5c/30/3b5c307c5f3b9bf12f89bf9b29f7eff0.jpg",
-  }
-];
   
 router.get('/crear', tropasController.get_crear);
-  
-router.post('/crear', (request, response, next) => {
-    console.log(request.body);
-    tropas.push({
-      clase: request.body.clase, 
-      nivel: 1, 
-      vida: request.body.vida, 
-      ataque: request.body.ataque, 
-      imagen: request.body.imagen,
-    });
-    response.redirect('/');
-});
-  
-router.get('/', (request, response, next) => {
-  response.render('clases', {
-    tropas: tropas,
-  });
-});
+router.post('/crear', tropasController.post_crear);
+router.get('/', tropasController.get_root);
 
 module.exports = router;
